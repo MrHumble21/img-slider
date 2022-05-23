c777e3e3c84dc7d4bac3fcecf5645314e7a60f6c
MrHumble21 <malikovabdulboriy010@gmail.com>
2022-05-23 13:53:14 +0500

    .

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import data from "./data";
 import SimpleImageSlider from "react-simple-image-slider";
 
 // 'src/components/assets/4.jpg'
-const images  =  data.map(img =>(
-    Object.assign({url:img})
-))
+const images = data.map((img) => Object.assign({ url: img }));
 
-console.log(images)
+console.log(images);
 function Main() {
-return(
+  return (
     <div className="container">
-    <SimpleImageSlider
-        width={'100%'}
-        height={'80vh'}
+      <SimpleImageSlider
+        width={"100%"}
+        height={"80vh"}
         images={images}
         showBullets={true}
         showNavs={true}
       />
     </div>
-)
+  );
 }
 
 export default Main;
